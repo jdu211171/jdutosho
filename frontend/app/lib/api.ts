0ba9d857e6e20e1a01791401bfd4736b02f664a4
jdu211171 <211171m@jdu.uz>
2024-12-07 18:04:59 +0500

    api.ts file cleaned

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,16 +1,5 @@
 import axios from 'axios'
-import { booksResponseSchema } from '~/components/book-table/schema'
 
 export const api = axios.create({
 	baseURL: process.env.API_URL,
 })
-
-export const fetchBooks = async (token: string, page: number = 1) => {
-	const response = await api.get(`/books?page=${page}`, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-
-	return booksResponseSchema.parse(response.data)
-}
